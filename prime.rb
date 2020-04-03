6bdb3b7c4966e8f80467c4682ec32c6022acf8d5
chris brady <chrisb3546@yahoo.com>
2020-04-03 16:01:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,8 @@ def prime?(integer)
   if integer == 0 || integer == 1 || integer < 0
     return false
   end 
-    
+    ##if integer % number between 2 and integer == 0
+      if integer %
+      return false
     
 end
